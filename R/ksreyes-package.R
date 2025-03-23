9166bb07e752c6d4f0f0e9c87a772b188949e7a1
Kenneth S. Reyes <kenneth.rejes@gmail.com>
2025-03-23 15:47:37 +0100

    convert to R package

---
+++ R/ksreyes-package.R
@@ -0,0 +1,53 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @import ggplot2
+#' @import gdidata
+#' @importFrom cowplot draw_label
+#' @importFrom cowplot ggdraw
+#' @importFrom cowplot plot_grid
+#' @importFrom dplyr arrange
+#' @importFrom dplyr between
+#' @importFrom dplyr bind_rows
+#' @importFrom dplyr case_when
+#' @importFrom dplyr count
+#' @importFrom dplyr desc
+#' @importFrom dplyr distinct
+#' @importFrom dplyr filter
+#' @importFrom dplyr full_join
+#' @importFrom dplyr group_by
+#' @importFrom dplyr inner_join
+#' @importFrom dplyr left_join
+#' @importFrom dplyr lag
+#' @importFrom dplyr mutate
+#' @importFrom dplyr n
+#' @importFrom dplyr pull
+#' @importFrom dplyr recode
+#' @importFrom dplyr rename
+#' @importFrom dplyr rowwise
+#' @importFrom dplyr select
+#' @importFrom dplyr slice_head
+#' @importFrom dplyr summarise
+#' @importFrom dplyr ungroup
+#' @importFrom forcats fct_rev
+#' @importFrom lubridate year
+#' @importFrom readr read_csv
+#' @importFrom readr write_csv
+#' @importFrom rlang .data
+#' @importFrom stringr str_detect
+#' @importFrom stringr str_extract
+#' @importFrom stringr str_glue
+#' @importFrom tibble add_row
+#' @importFrom tibble tibble
+#' @importFrom tidyr complete
+#' @importFrom tidyr drop_na
+#' @importFrom tidyr fill
+#' @importFrom tidyr pivot_longer
+#' @importFrom tidyr pivot_wider
+#' @importFrom tidyr replace_na
+#' @importFrom tsibble scale_x_yearquarter
+#' @importFrom tsibble yearmonth
+#' @importFrom tsibble yearquarter
+## usethis namespace: end
+NULL
